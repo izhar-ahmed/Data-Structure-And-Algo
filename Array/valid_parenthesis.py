0254870e6727852f5f7e24f9d54c404f2cca7916
Izhar Ahmed <izharahmed1280@gmail.com>
2024-08-28 11:01:35 +0530

    problem solved: valid parenthesis

---
+++ Array/valid_parenthesis.py
@@ -0,0 +1,36 @@
+def validParenthesis(input):
+    stack = []
+    if len(input) == 1:
+        return False
+    for i in input:
+        if i == ")":
+            if len(stack) == 0:
+                return False
+            elif stack[len(stack) - 1] == "(":
+                stack.pop()
+            else:
+                return False
+        if i == "]":
+            if len(stack) == 0:
+                return False
+            elif stack[len(stack) - 1] == "[":
+                stack.pop()
+            else:
+                return False
+        if i == "}":
+            if len(stack) == 0:
+                return False
+            elif stack[len(stack) - 1] == "{":
+                stack.pop()
+            else:
+                return False
+        if i == "(" or i == "[" or i == "{":
+            stack.append(i)
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+            
+
+print(validParenthesis("(("))
